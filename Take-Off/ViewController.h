6c5a62b07ff6d9dff3a75644f83ea33109fa38c5
Vangeli Ontiveros <vontiverosi@bancoazteca.com.mx>
2013-08-19 12:47:04 -0500

    Initial Commit

---
+++ Take-Off/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Take-Off
+//
+//  Created by VANGELI ONTIVEROS on 19/08/13.
+//  Copyright (c) 2013 com.vanyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Take-Off/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Take-Off
+//
+//  Created by VANGELI ONTIVEROS on 19/08/13.
+//  Copyright (c) 2013 com.vanyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
