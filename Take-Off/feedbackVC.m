067324257d4dbdd8ffbf32dd0745320f48eb4ed8
Vangeli Ontiveros <vontiverosi@bancoazteca.com.mx>
2013-08-20 11:13:56 -0500

    agregando gitignore

---
+++ Take-Off/feedbackVC.m
@@ -0,0 +1,38 @@
+//
+//  feedbackVC.m
+//  Take-Off
+//
+//  Created by VANGELI ONTIVEROS on 19/08/13.
+//  Copyright (c) 2013 com.vanyas. All rights reserved.
+//
+
+#import "feedbackVC.h"
+
+@interface feedbackVC ()
+
+@end
+
+@implementation feedbackVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
